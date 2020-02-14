3c0ad382edab7fc7f0654ee53c9b0b0ce26c697b
PATO <mupatt@gmail.com>
2020-02-14 14:18:12 +0300

    hello

--- app/src/main/java/com/example/edithna/MainActivity.java
+++ app/src/main/java/com/example/edithna/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.edithna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage (View view) {
+        EditText message = (EditText)findViewById(R.id.message);
+        Toast.makeText (this, "Sending message " + message.getText().toString(),
+                Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra("MESSAGE", message.getText().toString());
+        startActivity(intent);
+        message.setText("");
     }
 }
 
